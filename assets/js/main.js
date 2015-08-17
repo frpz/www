5220668c006738688bcce9a64449fd6f077f2b04
Francis <francis@frpz.fr>
2015-08-17 17:23:38 +0200

    modifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,176 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+/* Languages */
+var deflg = window.navigator.userLanguage || window.navigator.language;
+var lstlg = localStorage.getItem("language");
+//la lange par defaut est l'anglais, sauf si le nav est fr, ou la dernière langue utilisée etait fr
+var firstlg = (/^fr/.test(lstlg || deflg)) ? "fr" : "en";
+
+var LG_fr = {
+	language: "fr",
+	titre: "Freelance depuis 2012",
+	maindesc: [
+		"Fort d'une expérience de 12 ans dans les télécoms et réseaux, j'ai choisi de me reconvertir dans le développement. Ce n'était pas nouveau pour moi car dans chaque entreprise où j'ai travaillé, j'ai régulièrement fait du développement à plus ou moins grande échelle, afin de répondre aux problématiques internes.",
+		"Maintenant je propose mes services de développement d'interface web aux entreprises qui ont des besoins très spécifiques et pour lesquels elles ne trouvent pas de produits sur le marché. Mon expérience me permet de faire des propositions pertinentes, sans que l'entreprise n'aie besoin de formaliser ses attentes.",
+		"J'utilise les nouvelles technologies à ma disposition afin de faire des applications toujours plus performantes et ergonomiques. J'adapte des templates pour le design et je peux m'interfacer avec un Web Designer au besoin."
+	],
+	header: { fonction: "Développeur Web Fullstack"},
+	demo: {
+		titre: "Quelques exemples de mes réalisations",
+		desc: "Il s'agit ici d'un petit aperçu, pour plus d'informations, veuillez me contacter.",
+		foot: "La plupart de ces applications ont un backend en PHP et un frontend en HTML/Javascript. Je suis ouvert à toutes les nouveautés et je teste régulièrement de nouvelles technologies, afin de toujours apporter la meilleur expérience utilisateur à mes clients.",
+		articles: [
+			{ titre: "FireFlux", image: "fireflux", width: "12u",
+				desc: "Application de gestion de flux de firewalls. ",
+				list: [
+					"Affichage des flux entre les différents firewalls",
+					"Import dans la base de données des fichiers de configuration des firewalls",
+					"Génération automatique des configuration, et envoi sur les équipements",
+					"Gestion des droits utilisateurs (Active Directory)",
+					"Utilisation de templates de création de flux",
+					"Regroupement des flux par Projets"
+				] },
+			{ titre: "KalliGraph", image: "kalligraph", width: "12u",
+				desc: "Affichage de graphiques",
+				list: [
+					"Affichage de différents types de graphiques (Linear, Polar...) depuis la base de données",
+					"Export des graphs sous différents formats (pdf, jpeg, xml)",
+					"Multi-utilisateurs et multi-groupes"
+				]
+			},
+			{ titre: "ORO: Gestion de temps", image: "oro", width: "12u",
+				desc: "Application de Time Managment pour des équipes travaillant sur différents projets",
+				list: [
+					"Multi-utilisateurs",
+					"Génération de rapports d'activité",
+					"Edition des projets"
+				]
+			},
+			{ titre: "JPower", image: "jpower", width: "6u",
+				desc: "Interface de provisionning de serveur DNS 'PowerDns'"
+			},
+			{ titre: "Outils de gestion Classe 4", image: "class4", width: "6u",
+				desc: "Ensemble d'outils permettants d'administrer un réseau Voix",
+				list: [
+					"Configuration ENUM pour Class 4",
+					"Import et analyse de configuration de l'Acme Packet",
+					"Configuration des bases de données de routage"
+				]
+			}
+		]
+	},
+	contact : {
+		intro   : "N'hésitez pas à me contacter pour un devis gratuit, je vous répondrai rapidement, à votre convenance par mail ou par téléphone.",
+		nom     : "Nom",
+		email   : "Email",
+		societe : "Société",
+		tel     : "Téléphone (facultatif)",
+		message : "Message",
+		envoyer : "Envoyer",
+		encours : "Envoi en cours...",
+		envoiok : "<strong>Merci!</strong> Votre message a été envoyé. J'y répondrai dans les plus bref délais."
+	}
+};
+var LG_en = {
+	language: "en",
+	titre: "Freelance since 2012",
+	maindesc: [
+		"After 12 years in telecom and network engineering and devops, I have decided to switch to development. It wasn't all new to me because in every company I've worked for, I often had to do some code to adjust to the internal requirements.",
+
+		//"Fort d'une expérience de 12 ans dans les télécoms et réseaux, j'ai choisi de me reconvertir dans le développement. Ce n'était pas nouveau pour moi car dans chaque entreprise où j'ai travaillé, j'ai régulièrement fait du développement à plus ou moins grande échelle, afin de répondre aux problématiques internes.",
+
+		"Now, based in Paris (France), I develop web interfaces for companies with very specific needs for which they can't find any products on the market. My experience enables me to do many relevant propositions, without the need for the clients to write any specifications.",
+		//"Maintenant je propose mes services de développement d'interface web aux entreprises qui ont des besoins très spécifiques et pour lesquels elles ne trouvent pas de produits sur le marché. Mon expérience me permet de faire des propositions pertinentes, sans que l'entreprise n'aie besoin de formaliser ses attentes.",
+
+		"I use new technologies in order to make applications more effective and ergonomic. I adapt templates for design and I can also work with your web designer if necessary."
+		//"J'utilise les nouvelles technologies à ma disposition afin de faire des applications toujours plus performantes et ergonomiques. J'adapte des templates pour le design et je peux m'interfacer avec un Web Designer au besoin."
+	],
+	header: { fonction: "Fullstack Web Developer"},
+	demo: {
+		titre: "Some samples of my work",
+		desc: "It's only a small snoop of my work, for more information please contact me.",
+		foot: "Most of these applications have a PHP backend and a HTML/javascript frontend. I am open to new technologies that I test very often so I can always improve my customers' user experience.",
+		articles: [
+			{ titre: "FireFlux", image: "fireflux", width: "12u",
+				desc: "Firewall flows management webapp",
+				list: [
+					"Display links between each firewall",
+					//"Affichage des flux entre les différents firewalls",
+
+					"Import firewall settings into database",
+					//"Import dans la base de données des fichiers de configuration des firewalls",
+
+					"Auto generation of configurations and application on the equipment",
+					//"Génération automatique des configuration, et envoi sur les équipements",
+
+					"Rights management for users (Active Directory)",
+					//"Gestion des droits utilisateurs (Active Directory)",
+
+					"Use of flow creation templates",
+					//"Utilisation de templates de création de flux",
+
+					"Group flows by projects"
+					//"Regroupement des flux par Projets"
+				] },
+			{ titre: "KalliGraph", image: "kalligraph", width: "12u",
+				desc: "Graph display",
+				list: [
+					"Display of several graph types (Linear, Polar) and tables from database",
+					//"Affichage de différents types de graphiques (Linear, Polar...) et de tableaux depuis la base de données",
+
+					"Export graphs with different formats (pdf, jpeg, xml)",
+					//"Export des graphs sous différents formats (pdf, jpeg, xml)",
+
+					"Multi users and multi groups"
+					//"Multi-utilisateurs et multi-groupes"
+				]
+			},
+			{ titre: "ORO: Time mamagement", image: "oro", width: "12u",
+				desc: "Time management application for teams working on sereval projects",
+				list: [
+					"Multi users",
+					//"Multi-utilisateurs",
+
+					"Activity reports",
+					//"Génération de rapports d'activité",
+
+					"Project configuration"
+					//"Edition des projets"
+				]
+			},
+			{ titre: "JPower", image: "jpower", width: "6u",
+				desc: "Commissioning web interface for DNS server 'PowerDns'"
+			},
+			{ titre: "Class 4 management tools", image: "class4", width: "6u",
+				desc: "Set of tools to manage a voice network",
+				list: [
+					"ENUM config for the class 4",
+					//"Configuration ENUM pour Class 4",
+
+					"Importing and analysing of Acme Packet's configuration",
+					//"Import et analyse de configuration de l'Acme Packet",
+
+					"Commissionning of the routing databases"
+					//"Configuration des bases de données de routage"
+				]
+			}
+		]
+	},
+	contact : {
+		intro   : "Please don't hesitate to contact me for a free quotation, I will reply quickly, either by email or by phone as requested.",
+		nom     : "Name",
+		email   : "Email",
+		societe : "Company",
+		tel     : "Phone Number (optional)",
+		message : "Message",
+		envoyer : "Send",
+		encours : "Sending...",
+		envoiok : "<strong>Thank You!</strong> Your message has been sent. I will reply as soon as possible."
+	}
+};
+
+/* Main script */
 (function($) {
 
 	Handlebars.registerHelper('isFirst', function(context,opt) {
@@ -11,16 +181,36 @@
 		return context.data.first ? context.fn() : "";
 	});
 	
+	Handlebars.registerHelper('isLang', function(lg, titre, alt) {
+		console.log("helper isLang: ",arguments, this);
+		return new Handlebars.SafeString('<span class="lglink" title="'+alt+'">' + ( this.language == lg ? titre : '<a href="#/'+lg+'">'+titre+"</a>") + '</span>');
+	});
+	
+	//fonction pour compiler un template
 	var hb = function(template,opts){
 		var t= Handlebars.compile($("#"+template).html());
 		return t(opts || {});
 	};
 	var $main = $("#main");
+
+	//fonction pour préparer l'affichage d'un template
 	var display = function(template,opts){
 		return function(){
+			//On mémorise la dernire langue utilisée
+			localStorage.setItem("language", opts.language);
+			opts.header.language = opts.language;
+
+			//on recharge le header avec les textes de la langue
+			$("#header").html(hb("headerTemplate",opts.header));
+
+			//compilation du template principal
 			var t= Handlebars.compile($("#"+template).html());
+
+			//On fade le contenu actuel puis on remet le nouveau
 			$("#main").fadeOut(200,function(){
 				$("#main").html(t(opts || {})).fadeIn(200);
+
+				//galerie photo
 				$('#two').poptrox({
 					caption: function($a) { return $a.prev('h3').text(); },
 					overlayColor: '#2c2c2c',
@@ -35,23 +225,27 @@
 					windowMargin: (skel.breakpoint('small').active ? 0 : 50)
 				});
 
+				//formulaire de contact:
 				$("#contactform").submit(function(e){
 					var $f = $(e.currentTarget);
+					var subbut = $f.find("input[type=submit]").prop("disabled", true);
+					subbut.val(subbut.data("envoiencours"));
 					var ob = {};
 					$.each($f.serializeArray(), function(_, kv) {
 						ob[kv.name] = kv.value;
 					});
-					console.log("form: ", $f, ob);
+					//console.log("form: ", $f, ob);
 					$("#contactformerror").html('');
 					$.post($f.get(0).action, ob, function(data, res, xq){
-						console.log("form: ", $f.get(0).action, arguments);
-						if(data.error != 0) $("#contactformerror").html(data.message);
+						//console.log("form: ", $f.get(0).action, arguments);
+						if(data.error != 0){
+							$("#contactformerror").html(data.message);
+							subbut.prop("disabled", false).val(subbut.data("envoyer"));
+						}
 						if(data.error == 0){
 						$("#contactform").fadeOut(2000,function(){
-							$("#contactformerror").html('<div style="color: green; font-size: 1.3em; border: 2px dashed gray; padding: 30px; margin: auto;"><strong>Merci!</strong> Votre message a été envoyé. J\'y répondrai dans les plus bref délais.</div>');
-
+							$("#contactformerror").html($("#envoimessageok").html());
 							})
-
 						}
 					}, "jsonp");
 					return false;
@@ -62,60 +256,11 @@
 	};
 	
 	var router = new Simrou({
-		"/" : display("page-home", {
-			titre: "Freelance depuis 2012",
-			maindesc: [
-				"Fort d'une expérience de 12 ans dans les télécoms et réseaux, j'ai choisi de me reconvertir dans le développement. Ce n'était pas nouveau pour moi car dans chaque entreprise où j'ai travaillé, j'ai régulièrement fait du développement à plus ou moins grande échelle, afin de répondre aux problématiques internes.",
-				"Maintenant je propose mes services de développement d'interface web aux entreprises qui ont des besoins très spécifiques et pour lesquels elles ne trouvent pas de produits sur le marché. Mon expérience me permet de faire des propositions pertinentes, sans que l'entreprise n'aie besoin de formaliser ses attentes.",
-				"J'utilise les nouvelles technologies à ma disposition afin de faire des applications toujours plus performantes et ergonomiques. J'adapte des templates pour le design et je peux m'interfacer avec un Web Designer au besoin."
-			],
-			demo: {
-				titre: "Quelques exemples de mes réalisations",
-				desc: "Il s'agit ici d'un petit aperçu, pour plus d'informations, veuillez me contacter.",
-				foot: "La plupart de ces applications ont un backend en PHP et un frontend en HTML/Javascript. Je suis ouvert à toutes les nouveautés et je teste régulièrement de nouvelles technologies, afin de toujours apporter la meilleur expérience utilisateur à mes clients.",
-				articles: [
-					{ titre: "FireFlux", image: "fireflux", width: "12u",
-						desc: "Application de gestion de flux de firewalls. ",
-						list: [
-							"Affichage des flux entre les différents firewalls",
-							"Import dans la base de données des fichiers de configuration des firewalls",
-							"Génération automatique des configuration, et envoi sur les équipements",
-							"Gestion des droits utilisateurs (Active Directory)",
-							"Utilisation de templates de création de flux",
-							"Regroupement des flux par Projets"
-						] },
-					{ titre: "KalliGraph", image: "kalligraph", width: "12u",
-						desc: "Affichage de graphiques",
-						list: [
-							"Affichage de différents types de graphiques (Linear, Polar...) depuis la base de données",
-							"Export des graphs sous différents formats",
-							"Multi-utilisateurs et multi-groupes"
-						]
-					},
-					{ titre: "ORO: Gestion de temps", image: "oro", width: "12u",
-						desc: "Application de Time Managment pour des équipes travaillant sur différents projets",
-						list: [
-							"Multi-utilisateurs",
-							"Génération de rapports d'activité",
-							"Edition des projets"
-						]
-					},
-					{ titre: "JPower", image: "jpower", width: "6u",
-						desc: "Interface de provisionning de serveur DNS 'PowerDns'"
-					},
-					{ titre: "Outils de gestion Classe 4", image: "class4", width: "6u",
-						desc: "Ensemble d'outils permettants d'administrer un réseau Voix",
-						list: [
-							"Configuration ENUM pour Class 4",
-							"Import et analyse de configuration de l'Acme Packet",
-							"Configuration des bases de données de routage"
-						]
-					}
-				]
-			}
-		})
+		"/"   : display("page-home", LG_fr),
+		"/fr" : display("page-home", LG_fr),
+		"/en" : display("page-home", LG_en)
 	});
-	router.start("/");
+	router.start("/"+firstlg);
 
 
 
